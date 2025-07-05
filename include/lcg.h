9bc1ba51ba95d996cb16da459c97454f389d84db
ndr3www <ndr3.www.3@gmail.com>
2025-07-05 15:24:04 +0200

    make it a single header lib

--- include/lcg.h
+++ include/lcg.h
@@ -2,4 +2,14 @@
 
 #include <stdlib.h>
 
-void lcg_gen(__int128* dest, size_t n, long seed);
+const long M = 1L << 48L;  // 2^48
+const long A = 25214903917;
+const long C = 11;
+
+void lcg_gen(__int128* dest, size_t n, long seed) {
+  dest[0] = seed;
+
+  for (size_t i = 1; i < n; ++i) {
+    dest[i] = (dest[i - 1] * A + C) % M;
+  }
+}
