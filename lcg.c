4bc98518aee8f555757ca935b8f706193f5e3e0d
ndr3www <ndr3.www.3@gmail.com>
2025-08-19 15:51:54 +0200

    move lcg_gen definition and constants to a source file

---
+++ lcg.c
@@ -0,0 +1,13 @@
+#include "lcg.h"
+
+const long M = 1L << 48L;  // 2^48
+const long A = 25214903917;
+const long C = 11;
+
+void lcg_gen(__int128* dest, size_t n, long seed) {
+  dest[0] = seed;
+
+  for (size_t i = 1; i < n; ++i) {
+    dest[i] = (dest[i - 1] * A + C) % M;
+  }
+}
--- lcg.h
+++ lcg.h
@@ -2,14 +2,4 @@
 
 #include <stddef.h>
 
-const long M = 1L << 48L;  // 2^48
-const long A = 25214903917;
-const long C = 11;
-
-void lcg_gen(__int128* dest, size_t n, long seed) {
-  dest[0] = seed;
-
-  for (size_t i = 1; i < n; ++i) {
-    dest[i] = (dest[i - 1] * A + C) % M;
-  }
-}
+void lcg_gen(__int128* dest, size_t n, long seed);
