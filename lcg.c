35c2642a5709282912a29a918faee75cd5928324
ndr3www <ndr3.www.3@gmail.com>
2025-09-03 04:03:10 +0200

    seed is no longer the first number in the pseudorandom series

--- lcg.c
+++ lcg.c
@@ -1,9 +1,13 @@
 #include "lcg.h"
 
+unsigned _BitInt(128) lcg_proc(unsigned _BitInt(128) number) {
+  return (number * LCG_CONST_A + LCG_CONST_C) % LCG_CONST_M;
+}
+
 void lcg_gen(unsigned _BitInt(128)* dest, size_t n, long seed) {
-  dest[0] = seed;
+  dest[0] = lcg_proc(seed);
 
   for (size_t i = 1; i < n; ++i) {
-    dest[i] = (dest[i - 1] * LCG_CONST_A + LCG_CONST_C) % LCG_CONST_M;
+    dest[i] = lcg_proc(dest[i - 1]);
   }
 }
--- lcg.h
+++ lcg.h
@@ -7,4 +7,5 @@
 #define LCG_CONST_C 11
 #define LCG_RAND_MAX LCG_CONST_M - 1
 
+unsigned _BitInt(128) lcg_proc(unsigned _BitInt(128) number);
 void lcg_gen(unsigned _BitInt(128)* dest, size_t n, long seed);
