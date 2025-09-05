da698257f44f4ac2740de780263717872093c671
ndr3www <ndr3.www.3@gmail.com>
2025-09-05 15:16:03 +0200

    implement separate functions for generating integer and floating-point values, introduce negative numbers to the pseudorandom set - _BitInt(128) is no longer unsigned

--- lcg.c
+++ lcg.c
@@ -1,13 +1,31 @@
 #include "lcg.h"
 
-unsigned _BitInt(128) lcg_proc(unsigned _BitInt(128) number) {
+_BitInt(128) lcg_gen(_BitInt(128) number) {
   return (number * LCG_CONST_A + LCG_CONST_C) % LCG_CONST_M;
 }
 
-void lcg_gen(unsigned _BitInt(128)* dest, size_t n, long seed) {
-  dest[0] = lcg_proc(seed);
+void lcg_randi(int* dest, size_t n, long seed, long min, long max) {
+  _BitInt(128) numbers[n];
+  numbers[0] = lcg_gen(seed);
 
   for (size_t i = 1; i < n; ++i) {
-    dest[i] = lcg_proc(dest[i - 1]);
+    numbers[i] = lcg_gen(numbers[i - 1]);
+  }
+
+  for (size_t i = 0; i < n; ++i) {
+    dest[i] = min + numbers[i] / (LCG_RAND_MAX / (max - min));
+  }
+}
+
+void lcg_randf(float* dest, size_t n, long seed, float min, float max) {
+  _BitInt(128) numbers[n];
+  numbers[0] = lcg_gen(seed);
+
+  for (size_t i = 1; i < n; ++i) {
+    numbers[i] = lcg_gen(numbers[i - 1]);
+  }
+
+  for (size_t i = 0; i < n; ++i) {
+    dest[i] = min + (float)numbers[i] / ((float)LCG_RAND_MAX / (max - min));
   }
 }
--- lcg.h
+++ lcg.h
@@ -5,7 +5,8 @@
 #define LCG_CONST_M (1L << 48L)  // 2^48
 #define LCG_CONST_A 25214903917
 #define LCG_CONST_C 11
-#define LCG_RAND_MAX LCG_CONST_M - 1
+#define LCG_RAND_MAX (LCG_CONST_M - 1)
 
-unsigned _BitInt(128) lcg_proc(unsigned _BitInt(128) number);
-void lcg_gen(unsigned _BitInt(128)* dest, size_t n, long seed);
+_BitInt(128) lcg_gen(_BitInt(128) number);
+void lcg_randi(int* dest, size_t n, long seed, long min, long max);
+void lcg_randf(float* dest, size_t n, long seed, float min, float max);
