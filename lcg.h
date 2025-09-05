1dd85e58fdbcf1694749da9b7e274b59c0de325b
ndr3www <ndr3.www.3@gmail.com>
2025-09-05 20:16:44 +0200

    add documentation

--- lcg.h
+++ lcg.h
@@ -7,6 +7,13 @@
 #define LCG_CONST_C 11
 #define LCG_RAND_MAX (LCG_CONST_M - 1)
 
+// Core formula of the linear congruential generator
 _BitInt(128) lcg_gen(_BitInt(128) number);
+
+// Generates a pseudorandom series of integers of size n in the range [min, max)
+// and stores the results in the dest array
 void lcg_randi(int* dest, size_t n, long seed, int min, int max);
+
+// Generates a pseudorandom series of floating-point numbers of size n in the range (min, max)
+// and stores the results in the dest array
 void lcg_randf(float* dest, size_t n, long seed, float min, float max);
