a85c1193c6c69af86f6d62f9b7ebe0be897f745e
ndr3www <ndr3.www.3@gmail.com>
2025-09-05 19:56:08 +0200

    minor optimizations of the lcg_rand functions

--- lcg.c
+++ lcg.c
@@ -7,12 +7,10 @@ _BitInt(128) lcg_gen(_BitInt(128) number) {
 void lcg_randi(int* dest, size_t n, long seed, int min, int max) {
   _BitInt(128) numbers[n];
   numbers[0] = lcg_gen(seed);
-
+  dest[0] = min + numbers[0] / (LCG_RAND_MAX / (max - min));
+  
   for (size_t i = 1; i < n; ++i) {
     numbers[i] = lcg_gen(numbers[i - 1]);
-  }
-
-  for (size_t i = 0; i < n; ++i) {
     dest[i] = min + numbers[i] / (LCG_RAND_MAX / (max - min));
   }
 }
@@ -20,12 +18,10 @@ void lcg_randi(int* dest, size_t n, long seed, int min, int max) {
 void lcg_randf(float* dest, size_t n, long seed, float min, float max) {
   _BitInt(128) numbers[n];
   numbers[0] = lcg_gen(seed);
-
+  dest[0] = min + (float)numbers[0] / ((float)LCG_RAND_MAX / (max - min));
+  
   for (size_t i = 1; i < n; ++i) {
     numbers[i] = lcg_gen(numbers[i - 1]);
-  }
-
-  for (size_t i = 0; i < n; ++i) {
     dest[i] = min + (float)numbers[i] / ((float)LCG_RAND_MAX / (max - min));
   }
 }
